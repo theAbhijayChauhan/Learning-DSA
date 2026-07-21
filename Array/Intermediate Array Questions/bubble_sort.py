01fbf1ebee0c917344c6bc720cf8d7b9a9145950
theAbhijayChauhan <abhijaychauhan99@gmail.com>
2026-07-21 20:43:45 +0530

    Updated Code for bubble sort with descending order.

--- Array/Intermediate Array Questions/bubble_sort.py
+++ Array/Intermediate Array Questions/bubble_sort.py
@@ -1,11 +1,25 @@
 from array import *
+
+# Bubble Sort Ascending
+
 arr = array("i", [45,13,59,67,51,37])
 
-for i in range(len(arr)):
+for i in range(len(arr)-1):
     for j in range(len(arr)-i -1):
         if arr[j] > arr[j+1]:
             temp = arr[j]
             arr[j] = arr[j + 1]
+            arr[j + 1] = temp
+
+print(arr)
+
+# Bubble Sort Descending
+
+for i in range(len(arr)-1):
+    for j in range(len(arr)-i -1):
+        if arr[j] < arr[j+1]:
+            temp = arr[j]
+            arr[j] = arr[j+1]
             arr[j + 1] = temp
 
 print(arr)
